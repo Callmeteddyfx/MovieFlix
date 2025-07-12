1b860877736ef274755f17f4d63533bedc831baa
Callmeteddyfx <136664394+Callmeteddyfx@users.noreply.github.com>
2025-07-11 22:00:47 -0700

    browserrouter updated

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/MovieFlix/', // ← important!
   plugins: [react()],
   define: {
     'process.env': {} 
-  },
-  base: '/MovieFlix/', // ← important!
-  plugins: [react()],
+  }, 
 })
